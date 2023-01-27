86911d3ffc046a535f358d06cc73b964f2d9e5b3
megoshmego <108840586+megoshmego@users.noreply.github.com>
2023-01-27 18:49:46 -0500

    part II

--- flask-survey/app.py
+++ flask-survey/app.py
@@ -1,39 +1,58 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, redirect, flash, session
 from flask_debugtoolbar import DebugToolbarExtension
-from pprint import pprint
+from surveys import satisfaction_survey as survey
+
+RESPONSES_KEY = "responses"
 
 app = Flask(__name__)
-
-app.debug = True
-
 app.config['SECRET_KEY'] = "mego"
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
-toolbar = DebugToolbarExtension(app)
-
-@app.route('/')
-def home.html():
-    return render_template('home.html', survey=survey)
-
-@app.route('/question/quest_num')
-def get_question(quest_num):
-    question = survey.questions[quest_num]
-    return render_template(
-        "question.html", question_num=quest_num, question=question)
-
-
-input = request.args['answer']
+debug = DebugToolbarExtension(app)
 
 
 
+@app.route("/")
+def home_start():
+    return render_template("home.html", survey=survey)
 
 
 
+@app.route("/begin", methods=["POST"])
+def start_survey():
+    session[RESPONSES_KEY] = []    
+    return redirect("/questions/0")
 
 
+@app.route("/answer", methods=["POST"])
+def handle_question():
+    choice = request.form['answer']
+    responses = session[RESPONSES_KEY]
+    responses.append(choice)
+    session[RESPONSES_KEY] = responses
+    
+    if (len(responses) == len(survey.questions)):
+        return redirect("/complete")
+    else:
+        return redirect(f"/questions/{len(responses)}")
+    
+@app.route("/questions/<int:qid>")
+def show_question(qid):
+    responses = session.get(RESPONSES_KEY)
+    
+    if (responses is None):
+        return redirect("/")
+    
+    if (len(responses) == len(survey.questions)):
+        return redirect("/complete")
+    
+    if (len(responses) != qid):
+        flash(f"Invalid question id: {qid}.")
+        return redirect(f"/questions/{len(responses)}")
+    
+    question = survey.questions[qid]
+    return render_template("questions.html", question_num=qid, question=question) 
 
-
-
-
-
-
-responses = []
+@app.route("/complete")
+def complete():
+    return render_template("finish.html")   
--- flask-survey/surveys.py
+++ flask-survey/surveys.py
@@ -1,4 +1,17 @@
+from flask import Flask,redirect,url_for,render_template,request
 
+
+
+app=Flask(__name__)
+@app.route('/',methods=['GET','POST'])
+def home():
+    if request.method=='POST':
+        # Handle POST Request here
+        return render_template('base.html')
+    return render_template('base.html')
+
+if __name__ == '__main__':
+    app.run(port=5000,debug=True)
 
 
 class Question:
